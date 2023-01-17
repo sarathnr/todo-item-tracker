a54e389bd532c0ce18c37d2442e5f58e5c3f08ed
sarathnr <55869778+sarathnr@users.noreply.github.com>
2023-01-18 00:11:15 +0100

    Added job to update expired items

--- src/main/java/com/simplesystem/todoitems/dao/repository/TodoRepository.java
+++ src/main/java/com/simplesystem/todoitems/dao/repository/TodoRepository.java
@@ -5,8 +5,10 @@ import com.simplesystem.todoitems.enums.TodoStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    List<Todo> findByDueDateBeforeAndStatusNot(LocalDateTime now, TodoStatus pastDue);
 }
---
+++ src/main/java/com/simplesystem/todoitems/job/StatusUpdateJob.java
@@ -0,0 +1,33 @@
+package com.simplesystem.todoitems.job;
+
+import com.simplesystem.todoitems.dao.entity.Todo;
+import com.simplesystem.todoitems.dao.repository.TodoRepository;
+import com.simplesystem.todoitems.enums.TodoStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Component
+public class StatusUpdateJob {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Scheduled(cron = "0 */1 * ? * *")
+    public void updateExpiredItems() {
+        log.info("{} in progress", getClass().getSimpleName());
+
+        List<Todo> expiredItems = todoRepository.findByDueDateBeforeAndStatusNot(LocalDateTime.now(), TodoStatus.PAST_DUE);
+        expiredItems.forEach(item -> item.setStatus(TodoStatus.PAST_DUE));
+
+        todoRepository.saveAll(expiredItems);
+
+        log.info("{} items updated with PAST_DUE status - ", expiredItems.stream().count());
+        log.info("{} successfully completed.", getClass().getSimpleName());
+    }
+}
